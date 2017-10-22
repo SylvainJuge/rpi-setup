db4de81ae3ce3ea9c3687f810df1d16e20fa6a4b
Sylvain Juge <syl20j@gmail.com>
2017-10-22 15:08:55 +0200

    dht22 role & fix become

---
+++ roles/dht22/files/send-thingsboard.py
@@ -0,0 +1,60 @@
+import os
+import time
+import sys
+import Adafruit_DHT as dht
+import paho.mqtt.client as mqtt
+import json
+
+def get_env(v, default):
+    m = os.environ
+    if v in m:
+        return m[v]
+    else:
+        return default
+
+host = get_env('THINGSBOARD_HOST', 'localhost')
+port = get_env('THINGSBOARD_PORT', 1883)
+
+print port
+print host
+
+sys.exit()
+
+# Data capture and upload interval in seconds. Less interval will eventually hang the DHT22.
+INTERVAL=2
+
+sensor_data = {'temperature': 0, 'humidity': 0}
+
+next_reading = time.time() 
+
+client = mqtt.Client()
+
+# Set access token
+client.username_pw_set(ACCESS_TOKEN)
+
+# Connect to ThingsBoard using default MQTT port and 60 seconds keepalive interval
+client.connect(THINGSBOARD_HOST, THINGSBOARD_PORT, 60)
+
+client.loop_start()
+
+try:
+    while True:
+#        humidity,temperature = dht.read_retry(dht.DHT22, 4)
+#        humidity = round(humidity, 2)
+#        temperature = round(temperature, 2)
+        #print(u"Temperature: {:g} C, Humidity: {:g}%".format(temperature, humidity))
+#        sensor_data['temperature'] = temperature
+#        sensor_data['humidity'] = humidity
+
+        # Sending humidity and temperature data to ThingsBoard
+        #client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
+
+        next_reading += INTERVAL
+        sleep_time = next_reading-time.time()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+except KeyboardInterrupt:
+    pass
+
+client.loop_stop()
+client.disconnect()
